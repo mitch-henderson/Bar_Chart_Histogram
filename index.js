87d2e1c10b705da2bf79b7d7085a4c3fabfce8e3
Mitch Henderson <mitch.henderson@gmail.com>
2021-03-24 17:55:02 -0400

    added addl csvs

--- index.js
+++ index.js
@@ -1,68 +1,99 @@
 
 const svgHeight = 500
 const svgWidth = 800
-const margins = {top:30, bottom:50, left:50, right:30}
+const margins = { top: 30, bottom: 50, left: 50, right: 30 }
 const plotHeight = svgHeight - margins.top - margins.bottom
 const plotWidth = svgWidth - margins.left - margins.right
 
 const svg = d3.select("svg")
-.attr("width", svgWidth)
-.attr("height", svgHeight)
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    
+svg.append("text")
+    .text("Estimated Time for Business to Get Back on Track")
+    .attr("x", svgWidth/2)
+    .attr("y", 20)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 25)
+
+svg.append("text")
+    .text("Q157. Roughly, how long do you think it will take from today, for your business to get back on track?")
+    .attr("x", 10)
+    .attr("y", svgHeight - 10)
+    .attr("font-size", 10)
+
 Promise.all([
+    d3.csv("Data_Managing_Crisis_Wave_3_CLEAN.csv"),
     d3.csv("Data_Managing_Crisis_Wave_4_CLEAN.csv"),
     d3.csv("Data_Managing_Crisis_Wave_5_CLEAN.csv")
-    ]).then((dataSets)=>{
-    const data = dataSets[1]
-    const q157data = data.map((d)=>{return Number(d.Q157);})
-    console.log(q157data)
-    const countArray = [{count:0,key:"<=3 months"},{count:0,key:"4-6 months"},{count:0,key:"7-9 months"},{count:0,key:"10-12 months"},{count:0,key:"13-15 months"},{count:0,key:"16-18 months"},{count:0,key:">= 18 months"}]
-    q157data.forEach((d)=>{
-        if (d<4){
-            countArray[0].count+=1         
-        } else if (d<7){
-            countArray[1].count+=1
-        } else if (d<10){
-            countArray[2].count+=1
-        } else if (d<13){
-            countArray[3].count+=1
-        } else if (d<16){
-            countArray[4].count+=1
-        } else if (d<19){
-            countArray[5].count+=1
-        }else {
-            countArray[6].count+=1
-        }
+]).then((datas) => {
+    const percentageArrays = []
+    datas.forEach((data) => {
+        const q157data = data.map((d) => { return Number(d.Q157); })
+        console.log(q157data)
+        const countArray = [{ count: 0, key: "<=3 months" }, { count: 0, key: "4-6 months" }, { count: 0, key: "7-9 months" }, { count: 0, key: "10-12 months" }, { count: 0, key: "13-15 months" }, { count: 0, key: "16-18 months" }, { count: 0, key: ">= 18 months" }]
+        q157data.forEach((d) => {
+            if (d < 4) {
+                countArray[0].count += 1
+            } else if (d < 7) {
+                countArray[1].count += 1
+            } else if (d < 10) {
+                countArray[2].count += 1
+            } else if (d < 13) {
+                countArray[3].count += 1
+            } else if (d < 16) {
+                countArray[4].count += 1
+            } else if (d < 19) {
+                countArray[5].count += 1
+            } else {
+                countArray[6].count += 1
+            }
+        })
+        const percentageArray = countArray.map((d) => {
+
+            return {
+                percentage: (d.count / q157data.length) * 100,
+                key: d.key
+            }
+        })
+        percentageArrays.push(percentageArray)
     })
-    const percentageArray = countArray.map((d)=>{
-        
-        return {       
-            percentage:(d.count / q157data.length) * 100,
-            key:d.key
-        }
-    })
-    
+
+
     const xScale = d3.scaleBand()
-    .domain(percentageArray.map((d)=>{return d.key}))
-    .range([0,svgWidth - margins.right - margins.left])
+        .domain(percentageArrays[0].map((d) => { return d.key }))
+        .range([0, svgWidth - margins.right - margins.left])
     const yScale = d3.scaleLinear()
-    .domain([0,100])
-    .range([0,svgHeight - margins.top - margins.bottom])
+        .domain([0, 100])
+        .range([svgHeight - margins.top - margins.bottom, 0])
     svg.append("g")
-    .call(d3.axisBottom().scale(xScale))
-    .attr("transform",`translate(${margins.left},${margins.top+plotHeight})`)
+        .call(d3.axisBottom().scale(xScale))
+        .attr("transform", `translate(${margins.left},${margins.top + plotHeight})`)
 
 
     svg.append("g")
-    .call(d3.axisLeft().scale(yScale))
-    .attr("transform",`translate(${margins.left},${margins.top})`)
+        .call(d3.axisLeft().scale(yScale))
+        .attr("transform", `translate(${margins.left},${margins.top})`)
 
-    console.log(percentageArray)
-    svg.selectAll("rect")
-    .data(percentageArray)
-    .enter()
-    .append("rect")
-    .attr("x",(d)=>{return margins.left+xScale(d.key)})
-    .attr("y",(d)=>{return margins.top+plotHeight-yScale(d.percentage)})
-    .attr("width",50)
-    .attr("height",(d)=>{return yScale(d.percentage)})
+    const timeFramesPadding = 20   
+    const widthPerX = plotWidth / percentageArrays[0].length - timeFramesPadding
+    const widthPerRect = widthPerX / percentageArrays.length
+    const colorScale = ["#FF5B57", "#51E4EB", "#C2FB56"] //need to add colors if we add more data
+    console.log(percentageArrays[0])
+
+    percentageArrays.forEach((percentageArray,index) => {
+        svg.selectAll(`.rect${index}`)
+        .data(percentageArray)
+        .enter()
+        .append("rect")
+        .attr("x", (d) => { return margins.left + xScale(d.key) + index * widthPerRect + timeFramesPadding / 2 })
+        .attr("y", (d) => { return margins.top + yScale(d.percentage) })
+        .attr("width", widthPerRect)
+        .attr("height", (d) => { return plotHeight - yScale(d.percentage) })
+        .attr("class", `rect${index}`)
+        .attr("fill", colorScale[index])
+        .append("title")
+        .text((d)=> {return `percentage: ${Number.parseFloat(d.percentage).toPrecision(3)}`})
+    })
+
 })
